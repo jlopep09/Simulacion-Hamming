d130aaf8fce771a60f4a9211dbb76f8edc2ac8ff
Jose Antonio Lopez Perez <jose.lppz03@gmail.com>
2024-03-12 11:21:55 +0100

    Implemented checkInput to check for dupe element values

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ public class Main{
     public static void main(String[] args){
 
         System.out.println("_____________________________________________________");
-        String Alpha = "ABCDEFGHIJKLMNNOPQRSTUVWXYZ abcdefghiíjklmnñopqrstuvwxyz";
-        String binImput = "00000001100110010010101001011101001";
+        String Alpha = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ abcdefghiíjklmnñopqrstuvwxyz";
+        String binCodedInput = "00000001100110010010101001011101001";
         Controlador controlador = Controlador.getInstance();
+        controlador.setAlphabet(Alpha);
     }
 }
--- src/modelo/alphabet/Alphabet.java
+++ src/modelo/alphabet/Alphabet.java
@@ -7,6 +7,15 @@ public class Alphabet {
     }
     private void checkInput(String input){
         boolean valid = true;
+
+        for(int i = 0; i<input.length();i++){
+            if(input.indexOf(input.charAt(i))!=input.lastIndexOf(input.charAt(i))){
+                valid = false;
+                System.out.println(input.charAt(i));
+                System.out.println(input.indexOf(input.charAt(i)));
+                System.out.println(input.lastIndexOf(input.charAt(i)));
+            }
+        }
         if(!valid){
             throw new IllegalArgumentException("Alphabet input is not valid.");
         }
